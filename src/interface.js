5bcc71b14dcd59e2d2c9d95c57500236aeef2411
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-12-10 15:37:46 +0000

    in the process of correcting my buttons...don't judge me

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,20 @@
+
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  $('#temperature').text(thermostat.temperature);
+})
+
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('#temp-up').click(function() {
-    thermostat.increaseTemperature();
+  $('#temperature-up').click(function() {
+    thermostat.up();
     updateTemperature();
   });
 
-  $('#temp-down').click(function() {
-    thermostat.decreaseTemperature();
+  $('#temperature-down').click(function() {
+    thermostat.down();
     updateTemperature();
   });
 
@@ -16,20 +22,25 @@ $(document).ready(function() {
     thermostat.reset();
     updateTemperature();
   });
-
+  // $('#thermostatreset').on('click', function() {
+  // thermostat.reset();
+  // $('#thermostattemperature').text(thermostat.temperature);
+  // $('#thermostatenergyUsage').text(thermostat.energyUsage());
+  // });
   $('#psm-on').click(function() {
     thermostat.powerSavingModeOn();
     $('#power-saving').text('on')
     updateTemperature();
-  })
+  });
 
   $('#psm-off').click(function() {
     thermostat.powerSavingModeOff();
     $('#power-saving').text('off')
     updateTemperature();
-  })
+  });
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
+  $('#temperature').text(thermostat.temperature);
+  $('#temperature').attr('class', thermostat.energyUsage());
   };
 });
